34424b9893696221b2a8fcd4e8a7417c500b4d08
Rikki Kendall <yrk3@duke.edu>
2020-08-09 03:45:43 -0400

    first ocmmit

---
+++ precache-manifest.057f64cbb59c52ec28bb2a48555b2e19.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07f24c9acc5117b8844f8a93f0504f29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cfc7773e3892842812a",
+    "url": "/static/css/2.0fd4d61c.chunk.css"
+  },
+  {
+    "revision": "c1c22e8117ffc6275fc9",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "9cfc7773e3892842812a",
+    "url": "/static/js/2.ca82c53e.chunk.js"
+  },
+  {
+    "revision": "92204df3b82ea0ddcb32ac6c9fde9935",
+    "url": "/static/js/2.ca82c53e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1c22e8117ffc6275fc9",
+    "url": "/static/js/main.52a061bb.chunk.js"
+  },
+  {
+    "revision": "352353811d42eee32704",
+    "url": "/static/js/runtime-main.e49ee613.js"
+  },
+  {
+    "revision": "cdce653425d7cb1f61fdbea7166a2a59",
+    "url": "/static/media/statfinalproj.cdce6534.pdf"
+  }
+]);
